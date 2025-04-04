33b853edcd4d4fe5e3651edadf140d7f950ebaa5
Jessica Anderson <jespaiand@hotmail.com>
2025-04-04 18:11:48 -0500

    eedit

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,20 @@
 from flask import Flask
-from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
-app.config.from_object('config')
+db = SQLAlchemy()
+migrate = Migrate()
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config') 
 
-csrf = CSRFProtect(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-from app import views
+    csrf = CSRFProtect(app)
+
+    from app import views
+
+    return app
